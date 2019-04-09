ecd763bbb08d4d473b2bf57ed31e2c335bea98b6
denadan <denadan.sa@gmail.com>
2019-04-09 21:21:08 +0300

    remove logging

--- Logger.cs
+++ Logger.cs
@@ -10,45 +10,45 @@ namespace ArmorRepair
 
         public Logger()
         {
-            using (StreamWriter streamWriter = new StreamWriter(path, false))
-            {
-                streamWriter.WriteLine("(NFO) " + DateTime.Now.ToString() + " - Logger started.");
-            }
+            //using (StreamWriter streamWriter = new StreamWriter(path, false))
+            //{
+            //    streamWriter.WriteLine("(NFO) " + DateTime.Now.ToString() + " - Logger started.");
+            //}
         }
 
         public static void LogError(Exception ex)
         {           
-            using (StreamWriter streamWriter = new StreamWriter(path, true))
-            {
-                streamWriter.WriteLine("(ERR) ");
-                streamWriter.WriteLine(string.Concat(new string[]
-                {
-                    "Message :",
-                    ex.Message,
-                    Environment.NewLine,
-                    "Stacktrace :",
-                    ex.StackTrace
-                }) + Environment.NewLine + "Date :" + DateTime.Now.ToString());
-                streamWriter.WriteLine(Environment.NewLine + "------------------------------------------------------------------------" + Environment.NewLine);
-            }
+            //using (StreamWriter streamWriter = new StreamWriter(path, true))
+            //{
+            //    streamWriter.WriteLine("(ERR) ");
+            //    streamWriter.WriteLine(string.Concat(new string[]
+            //    {
+            //        "Message :",
+            //        ex.Message,
+            //        Environment.NewLine,
+            //        "Stacktrace :",
+            //        ex.StackTrace
+            //    }) + Environment.NewLine + "Date :" + DateTime.Now.ToString());
+            //    streamWriter.WriteLine(Environment.NewLine + "------------------------------------------------------------------------" + Environment.NewLine);
+            //}
         }
 
         public static void LogInfo(string line)
         {
-            using (StreamWriter streamWriter = new StreamWriter(path, true))
-            {
-                streamWriter.WriteLine("(NFO) " + DateTime.Now.ToString() + " - " + line);
-            }
+            //using (StreamWriter streamWriter = new StreamWriter(path, true))
+            //{
+            //    streamWriter.WriteLine("(NFO) " + DateTime.Now.ToString() + " - " + line);
+            //}
         }
 
         public static void LogDebug(string line)
         {
-            if(ArmorRepair.ModSettings.Debug) { 
-                using (StreamWriter streamWriter = new StreamWriter(path, true))
-                {
-                    streamWriter.WriteLine("(DBG) " + DateTime.Now.ToString() + " - " + line);
-                }
-            }
+            //if(ArmorRepair.ModSettings.Debug) { 
+            //    using (StreamWriter streamWriter = new StreamWriter(path, true))
+            //    {
+            //        streamWriter.WriteLine("(DBG) " + DateTime.Now.ToString() + " - " + line);
+            //    }
+            //}
         }
     }
 }
